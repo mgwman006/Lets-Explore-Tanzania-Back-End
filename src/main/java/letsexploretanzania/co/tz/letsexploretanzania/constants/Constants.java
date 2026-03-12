b5aeabb610db850974ae3511296db5ed26014202
Maneno Abdallah Mgwami <maneno@anylytical.co.za>
2026-03-12 05:46:54 +0200

    migrating s3

---
+++ src/main/java/letsexploretanzania/co/tz/letsexploretanzania/constants/Constants.java
@@ -0,0 +1,8 @@
+package letsexploretanzania.co.tz.letsexploretanzania.constants;
+
+public class Constants
+{
+  public static final String BUCKET_NAME = "letsexploretanzania-s3";
+
+  private Constants(){}
+}
